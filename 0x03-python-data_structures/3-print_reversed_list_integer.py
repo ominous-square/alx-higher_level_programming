0f8e0f2bd2da692a2cc4355b456912732fb2ef52
ominous-square <bnkioko@LTTECHMAC808.local>
2023-12-25 20:15:33 +0300

    Print one by one item

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return
     my_new_list=my_list[::-1]
-    print (my_new_list)
+    for i in my_new_list:
+       print("{:d}".format(i))
