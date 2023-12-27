33a4bf23f01bfbc3c3983917ba3b3fce8ef0164d
ominous-square <bnkioko@LTTECHMAC808.local>
2023-12-27 21:34:51 +0300

    Pycodestyle correction

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # tuple_a=[a, b]
     # tuple_b=[c,d]
     # tuple_c=[(a+c),(b+d)]
-    a, b, c, d = 0
+    a = 0
+    b = 0
+    c = 0
+    d = 0
     if len(tuple_a) >= 2:
         a = tuple_a[0]
         c = tuple_a[1]
@@ -14,6 +17,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a = tuple_a[0]
     if len(tuple_b) == 1:
         b = tuple_b[0]
-
     tuple_c = (a + b, c + d)
     return tuple_c
